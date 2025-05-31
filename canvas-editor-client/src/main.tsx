73800798ca8c06d7b295d2c86019546c02d823d2
alamin147 <alamin.14780@gmail.com>
2025-05-31 22:10:47 +0600

    redux toolkit, react router dom and other packages

--- canvas-editor-client/src/main.tsx
+++ canvas-editor-client/src/main.tsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { persistor, store } from './redux/store.ts'
+import { Provider } from 'react-redux'
+import { Toaster } from 'react-hot-toast'
+import { PersistGate } from 'redux-persist/integration/react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router=createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <div>404 Not Found</div>,
+    }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+ <Provider store={store}>
+      <Toaster />
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router}></RouterProvider>
+      </PersistGate>
+    </Provider>
   </StrictMode>,
 )
---
+++ canvas-editor-client/src/redux/api/baseApi.ts
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
+import { configENV } from "@/config";
+
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: fetchBaseQuery({
+    // Uncomment the line below to use the local server URL
+    // baseUrl: `${configENV.server_local_url}/api/v1`,
+    // Uncomment the line below to use the production server URL
+    baseUrl: `${configENV.server_prod_url}/api/v1`,
+    credentials: "include",
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+
+      if (token) {
+        headers.set("authorization", `${token}`);
+      }
+      return headers;
+    },
+  }),
+  tagTypes: [],
+
+  endpoints: () => ({}),
+});
