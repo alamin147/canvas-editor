26c1309f59a04c62288c5e41ac577d9128221ef1
alamin147 <alamin.14780@gmail.com>
2025-06-08 18:14:18 +0600

    client project apis

---
+++ canvas-editor-client/src/redux/features/project/projectApi.ts
@@ -0,0 +1,64 @@
+import { baseApi } from '../../api/baseApi';
+
+const projectApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    // Create a new project
+    createProject: builder.mutation({
+      query: (data) => ({
+        url: '/projects',
+        method: 'POST',
+        body: data,
+      }),
+      invalidatesTags: ['Projects'],
+    }),
+
+    // Get all projects
+    getAllProjects: builder.query({
+      query: () => ({
+        url: '/projects',
+        method: 'GET',
+      }),
+      providesTags: ['Projects'],
+    }),
+
+    // Get a single project by ID
+    getProject: builder.query({
+      query: (id) => ({
+        url: `/projects/${id}`,
+        method: 'GET',
+      }),
+      providesTags: (_result, _error, id) => [{ type: 'Projects', id }],
+    }),
+
+    // Update a project
+    updateProject: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `/projects/${id}`,
+        method: 'PATCH',
+        body: data,
+      }),
+      invalidatesTags: (_result, _error, { id }) => [
+        { type: 'Projects', id },
+        'Projects',
+      ],
+    }),
+
+    // Delete a project
+    deleteProject: builder.mutation({
+      query: (id) => ({
+        url: `/projects/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['Projects'],
+    }),
+  }),
+  overrideExisting: false,
+});
+
+export const {
+  useCreateProjectMutation,
+  useGetAllProjectsQuery,
+  useGetProjectQuery,
+  useUpdateProjectMutation,
+  useDeleteProjectMutation,
+} = projectApi;
