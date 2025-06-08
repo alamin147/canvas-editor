7e075d1aa720475eddaa37e8eea2d1520dbc0811
alamin147 <alamin.14780@gmail.com>
2025-06-08 18:04:37 +0600

    project validation zod

---
+++ canvas-editor-server/src/app/modules/project/project.validation.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod';
+
+const createProjectValidationSchema = z.object({
+  body: z.object({
+    title: z.string({ required_error: 'Project title is required.' }),
+    canvasData: z.string({ required_error: 'Canvas data is required.' }),
+    backgroundColor: z.string().optional(),
+    width: z.number().optional(),
+    height: z.number().optional(),
+  }).strict(),
+});
+
+const updateProjectValidationSchema = z.object({
+  body: z.object({
+    title: z.string().optional(),
+    canvasData: z.string().optional(),
+    backgroundColor: z.string().optional(),
+    width: z.number().optional(),
+    height: z.number().optional(),
+  }).strict(),
+});
+
+export const ProjectValidation = {
+  createProjectValidationSchema,
+  updateProjectValidationSchema,
+};
